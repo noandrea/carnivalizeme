bac356b4a25564884bfc226a4c15bcccc925d10e
Enrico Icardi <enrico.icardi@gmail.com>
2014-04-21 21:04:29 +0200

    injecting ads in the carnival pics array

--- app/js/controllers/simply_pages_ctrl.js
+++ app/js/controllers/simply_pages_ctrl.js
@@ -14,15 +14,7 @@ angular.module("app").controller('CarnivalCtrl', function($scope, Photos) {
     
     $scope.pageClass = 'page-carnival';
     $scope.photos = [];
-/*
-    $scope.photos.push({"id" : "01","audience": 0,"tags": ["black","white"],"added": "2014-03-30T14:08:49.613337", "stillimage": "http://bestclipartblog.com/clipart-pics/-test-clipart-9.jpg", "image": "http://upload.wikimedia.org/wikipedia/commons/0/0e/Pentagonalicositetrahedroncw.gif","up": 4,"dw": 0,"photo_count":0});
-    $scope.photos.push({"id" : "02","audience": 0,"tags": ["black","white"],"added": "2014-03-30T14:08:49.613337", "stillimage": "http://bestclipartblog.com/clipart-pics/-test-clipart-9.jpg","image": "http://upload.wikimedia.org/wikipedia/commons/5/50/Triple-Spiral-Labyrinth-animated.gif","up": 4,"dw": 0,"photo_count":0});
-    $scope.photos.push({"id" : "03","audience": 0,"tags": ["black","white"],"added": "2014-03-30T14:08:49.613337", "stillimage": "http://bestclipartblog.com/clipart-pics/-test-clipart-9.jpg","image": "http://i.imgur.com/4frTEpP.gif","up": 4,"dw": 0,"photo_count":0});
-    $scope.photos.push({"id" : "04","audience": 0,"tags": ["black","white"],"added": "2014-03-30T14:08:49.613337", "stillimage": "http://bestclipartblog.com/clipart-pics/-test-clipart-9.jpg","image": "http://upload.wikimedia.org/wikipedia/commons/5/50/Triple-Spiral-Labyrinth-animated.gif","up": 4,"dw": 0,"photo_count":0});
-    $scope.photos.push({"id" : "05","audience": 0,"tags": ["black","white"],"added": "2014-03-30T14:08:49.613337", "stillimage": "http://bestclipartblog.com/clipart-pics/-test-clipart-9.jpg","image": "https://lh5.googleusercontent.com/-jfmc_gfxbio/AAAAAAAAAAI/AAAAAAAAAr0/C7TKRSn7oD4/photo.jpg","up": 4,"dw": 0,"photo_count":0});
-    $scope.photos.push({"id" : "06","audience": 0,"tags": ["black","white"],"added": "2014-03-30T14:08:49.613337", "stillimage": "http://bestclipartblog.com/clipart-pics/-test-clipart-9.jpg","image": "http://upload.wikimedia.org/wikipedia/commons/5/50/Triple-Spiral-Labyrinth-animated.gif","up": 4,"dw": 0,"photo_count":0});
-*/
-
+    
     var filter = {};
     $scope.getPhotos = function (){ //TODO: here i can paginate passing "page"
         
@@ -31,7 +23,10 @@ angular.module("app").controller('CarnivalCtrl', function($scope, Photos) {
             if(response.length){
                 console.log('THE PHOTOS:', response);
                 $scope.photos = response;
-
+                //add 3 ads on random position between 0 and 11
+                $scope.photos.splice(Math.floor(Math.random()*5)+1, 0, {ad:true});
+                $scope.photos.splice(Math.floor(Math.random()*5)+6, 0, {ad:true});
+                $scope.photos.splice(Math.floor(Math.random()*5)+11, 0, {ad:true});
                 console.log($scope.photos);
 
             }else{
