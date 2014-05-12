eb154ea7b5f5bfd59e08b29d8955c0eb7341a449
Enrico Icardi <enrico.icardi@gmail.com>
2014-05-12 20:18:50 +0200

    ~ updated translations ~ text now allows HTML

--- app/js/app.js
+++ app/js/app.js
@@ -90,3 +90,13 @@ angular.module("app").filter('bytes', function() {
         return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
     };
 });
+
+//nl2br for strings
+angular.module("app").filter('nl2br', function($sce){
+    return function(msg,is_xhtml) { 
+        is_xhtml = is_xhtml || true;
+        var breakTag = (is_xhtml) ? '<br />' : '<br>';
+        msg = (msg + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
+        return $sce.trustAsHtml(msg);
+    };
+});
