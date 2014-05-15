78bd532e96a8e0d501204675b0e039f4ea6a2272
Enrico Icardi <enrico.icardi@gmail.com>
2014-05-15 15:53:41 +0200

    - deleted useless directives

--- app/js/directives/overlay-face.js
+++
@@ -1,7 +0,0 @@
-angular.module("app").directive('overlayFace', function() {
-  return {
-    restrict: "E",
-    replace: true,
-    templateUrl: 'overlay_face.html'
-  };
-});
--- app/js/directives/video-html5.js
+++
@@ -1,7 +0,0 @@
-angular.module("app").directive('videoHtml5', function() {
-  return {
-    restrict: "E",
-    replace: true,
-    templateUrl: 'video_html5.html'
-  };
-});
