6aa9c1cdb06ccaf9d59a786661401bdb9087052e
andrea giacobino <no.andrea@gmail.com>
2014-08-24 17:44:55 +0300

    * workaround for search by tags, the photos will be returned first sorted by tags (then up_vote and date) because the limitation in nab query engine

--- handlers/photos/photos_handler.py
+++ handlers/photos/photos_handler.py
@@ -32,19 +32,23 @@ class PhotoHandler(webapp2.RequestHandler):
         pagination_size = 32
 
         # this is the query
-        photo_query_fwd = Photo.query().order(-Photo.up_vote, -Photo.added)
-        photo_query_bkw = Photo.query().order(Photo.up_vote, Photo.added)
+        photo_query_fwd = Photo.query()#.order(-Photo.up_vote, -Photo.added)
+        photo_query_bkw = Photo.query()#.order(Photo.up_vote, Photo.added)
 
         query_tags = self.request.get('tags',default_value=None) # filter by tags if necessary
         if query_tags is not None:
             tags = [Tag.sanitize(x) for x in query_tags.split(',')]
             for tag in tags:
-                photo_query_fwd.filter(ndb.AND(Photo._properties['tags'] >= tag), Photo._properties['tags'] <= unicode(tag) + u'\ufffd')
-                photo_query_bkw.filter(ndb.AND(Photo._properties['tags'] >= tag), Photo._properties['tags'] <= unicode(tag) + u'\ufffd')
+                photo_query_fwd = photo_query_fwd.filter(ndb.AND(Photo._properties['tags'] >= tag), Photo._properties['tags'] <= unicode(tag) + u'\ufffd')
+                photo_query_bkw = photo_query_bkw.filter(ndb.AND(Photo._properties['tags'] >= tag), Photo._properties['tags'] <= unicode(tag) + u'\ufffd')
 
-            # photo_query_fwd = Photo.query(Photo.tags.IN(tags)).order(-Photo.up_vote, -Photo.added)
-            # photo_query_bkw = Photo.query(Photo.tags.IN(tags)).order(Photo.up_vote, Photo.added)
-
+            # sorting
+            photo_query_fwd = photo_query_fwd.order(Photo.tags, -Photo.up_vote, -Photo.added)
+            photo_query_bkw = photo_query_bkw.order(Photo.tags, Photo.up_vote, Photo.added)
+        else:
+            # sorting
+            photo_query_fwd = photo_query_fwd.order(-Photo.up_vote, -Photo.added)
+            photo_query_bkw = photo_query_bkw.order(Photo.up_vote, Photo.added)
 
         # current cursor
         current_cursor = ndb.Cursor(urlsafe=self.request.get('cr',default_value=None))  
